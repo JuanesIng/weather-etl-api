746135151dc66cadc54cf967ee4b9810e27c147a
JuanesIng <juanesing222@gmail.com>
2026-03-29 18:57:30 -0500

    Change in the limit to avoid redundancy

--- api/routers/weather.py
+++ api/routers/weather.py
@@ -8,10 +8,10 @@ import models
 router = APIRouter(prefix="/weather", tags=["weather"])
 
 @router.get("/latest", response_model= List[WeatherSchema], summary="List last records")
-def list_last(limit: int = 10, db: Session = Depends(get_db)):
+def list_last(db: Session = Depends(get_db)):
     return (db.query(models.Weather)
             .order_by(models.Weather.time_stamp.desc())
-            .limit(limit)
+            .limit(10)
             .all()
             )
 
