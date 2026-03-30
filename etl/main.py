9f6411c2246d564f61d27f46925b4076ff1f67d9
JuanesIng <juanesing222@gmail.com>
2026-03-29 19:04:29 -0500

    main.py location changed into etl directory

---
+++ etl/main.py
@@ -0,0 +1,24 @@
+import requests
+from dotenv import load_dotenv
+from os import getenv
+import extract, transform, load
+
+#Load environment variables from .env file
+load_dotenv()
+API_KEY = getenv('API_KEY')
+
+cities = ["Bogota", "Medellin", "Cali", "Bucaramanga", "Tunja"]
+
+data_city = [ ]
+
+for city in cities:
+    try:
+        data = extract.extract_weather(city, API_KEY)
+        
+        data_city.append(transform.transform_weather(data))
+
+    except requests.exceptions.RequestException:
+        print(f"The city {city} does not exist or did not load correctly")
+
+
+load.load_weather(data_city)
